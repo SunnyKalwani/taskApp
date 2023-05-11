20b8bf07234205f992d59481338670bddc33b2e1
Sunny K <sunnydkal@gmail.com>
2023-05-11 14:55:50 -0400

    Data displaying and directives

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  photo: string = "https://cdn.pixabay.com/photo/2023/05/03/09/16/rooster-7967053_640.jpg";
+
+}
